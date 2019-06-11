517f19ad3d9a4c0bf5b81af0ea4abe428b1a1a83
tishksenia <tishksenia@yandex.ru>
2019-06-11 20:50:41 +0300

    phone number is being formatted at the end of input

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,29 +14,33 @@ class Form extends React.Component {
     handleInputChange = event => {
         const name = event.target.name;
         let value = event.target.value;
+        
+        // format received data if it came from "phone" input
         if(name === "phone") {
             value = this.getFormattedNumber(value);
-            
         }
+
         this.setState({
             [name]: value
         });
     }
-    //!!!
     //Returns phone number in +7 (999) 999-11-11 format
     getFormattedNumber(num) {
-        let result = num.replace(/[^0123456789]/g, "");
-        
-        // let code = value.slice(0, 3);
-        // let num = `(${code})`;
-        // if(value.length > 3) {
-        //     num = `(${code}) ${value.slice(3, 6)}`;
+        // let numInDigits = num.replace(/[^0123456789]/g, "");
+        // let result = "";
+        // if(numInDigits.length <= 3) {
+        //     result = `(${numInDigits.slice(0,3)})`;
         // }
-        // else if(value.length > 5) {
-        //     num = `(${code}) ${value.slice(3, 6)}-${value.slice(6, 8)}-${value.slice(8, 10)}`;
+        // else if(numInDigits.length <= 6) {
+        //     result = `(${numInDigits.slice(0,3)}) ${numInDigits.slice(3, 6)}`;
         // }
-        
-        return result;
+        // else if(numInDigits.length <= 8) {
+        //     result = `(${numInDigits.slice(0,3)}) ${numInDigits.slice(3, 6)}-${numInDigits.slice(6, 8)}`;
+        // }
+        // else {
+        //     result = `(${numInDigits.slice(0,3)}) ${numInDigits.slice(3, 6)}-${numInDigits.slice(6, 8)}-${numInDigits.slice(8, 10)}`;
+        // }
+        return num.replace(/^(\+7|7|8?)(\d{3})(\d{3})(\d{2})(\d{2})/g, "+7 ($2) $3-$4-$5");
     }
     // Produces timestamp using current time and passes all the arguments to App > addNewAdItem()
     handleFormSubmit = event => {
@@ -65,15 +69,15 @@ class Form extends React.Component {
                 <form onSubmit={this.handleFormSubmit}>
                     <label>
                         Title
-                        <input name="title" type="text" value={this.state.title} onChange={this.handleInputChange} ></input>
+                        <input name="title" type="text" value={this.state.title} onChange={this.handleInputChange} maxLength="140" required></input>
                     </label>
                     <label>
                         Message
-                        <input name="message" type="text" value={this.state.message} onChange={this.handleInputChange} ></input>
+                        <input name="message" type="text" value={this.state.message} onChange={this.handleInputChange} maxLength="300"></input>
                     </label>
                     <label>
                         Phone
-                        <input name="phone" type="text" value={this.state.phone} onChange={this.handleInputChange} ></input>
+                        <input name="phone" type="tel" value={this.state.phone} onChange={this.handleInputChange} maxLength="18" pattern="^(\+7|7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$" placeholder="+7 (111) 111-11-11" required></input>
                     </label>
                     <input type="submit" value="Add" />
                 </form>
