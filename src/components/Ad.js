8ea2cdb0fc51980f55783bd425092f3ece0456e0
tishksenia <tishksenia@yandex.ru>
2019-06-14 09:19:10 +0300

    Ad CSS

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -5,30 +5,33 @@ class Ad extends React.Component {
   render() {
     return (
       <div className="Ad">
-        <h3 className="Ad-title">{this.props.title}</h3>
-        {this.props.picture ? (
-          <img
-            src={`data:image/png;base64,${this.props.picture}`}
-            alt={this.props.title}
-            crossorigin="anonymous"
-          />
-        ) : (
-          ""
-        )}
-        <p className="Ad-message">{this.props.message}</p>
-        <span className="Ad-phone">Phone: {this.props.phone}</span>
-        {this.props.city ? (
-          <span className="Ad-city">City: {this.props.city}</span>
-        ) : (
-          ""
-        )}
+        <div className="Ad-information">
+          <h3 className="Ad-title">{this.props.title}</h3>
+          {this.props.picture ? (
+            <img
+              src={`data:image/png;base64,${this.props.picture}`}
+              alt={this.props.title}
+              crossorigin="anonymous"
+            />
+          ) : (
+            ""
+          )}
+          <p className="Ad-message">{this.props.message}</p>
+        </div>
+        <div className="Ad-contacts-and-controls">
+          <p className="Ad-phone">{this.props.phone}</p>
+          {this.props.city ? <p className="Ad-city">{this.props.city}</p> : ""}
 
-        <button
-          className="Ad-delete-btn btn"
-          onClick={() => this.props.deleteHandler(this.props.dateTimestamp)}
-        >
-          Delete Ad
-        </button>
+          <div className="controls">
+            <button className="Ad-edit-btn Ad-btn btn">Редактировать</button>
+            <button
+              className="Ad-delete-btn Ad-btn btn"
+              onClick={() => this.props.deleteHandler(this.props.dateTimestamp)}
+            >
+              Удалить
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
