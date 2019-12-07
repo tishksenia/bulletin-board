b2a41771b36f4aff8ff6fef548e0e2180720630a
Ksenia Tishchenko <tishksenia@yandex.ru>
2019-12-07 10:14:35 +0300

    App, Form, FormErrors

--- src/App.js
+++ src/App.js
@@ -1,85 +1,86 @@
-import React from "react";
-import AdsList from "./components/AdsList/AdsList";
-import Form from "./components/Form";
-import "./css/App.css";
+import React from 'react';
+import AdsList from './components/AdsList/AdsList';
+import Form from './components/Form/Form';
+import 'reset-css';
+import './App.scss';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.appTitle = "Bulletin Board";
-    this.state = {
-      AdsArray: []
-    };
-  }
-  componentDidMount() {
-    this.retrieveFromLocalStorage();
-  }
-  componentDidUpdate() {
-    this.saveToLocalStorage();
-  }
-  // Save AdsArray to the local storage, using "AdsArray" key
-  saveToLocalStorage() {
-    const ads = this.state.AdsArray;
-    localStorage.setItem("AdsArray", JSON.stringify(ads));
-  }
-  // Retrieves / initializes (if no data was retrieved) AdsArray
-  retrieveFromLocalStorage() {
-    let ads = [];
-    let adsStoredValue = localStorage.getItem("AdsArray");
-    //if we were able to get stored values
-    if (adsStoredValue) {
-      ads = JSON.parse(adsStoredValue);
+    constructor(props) {
+        super(props);
+        this.appTitle = 'Bulletin Board';
+        this.state = {
+            AdsArray: []
+        };
     }
-    this.setState({ AdsArray: ads });
-  }
-  // Removes ALL entries from local storage AND from state
-  clearLocalStorage() {
-    this.setState({ AdsArray: [] });
-    localStorage.clear();
-  }
-  // Handle form submit, adding new <Ad /> element (with given arguments) to the AdsArray
-  addNewAdItem = (title, message, phone, city, dateTimestamp) => {
-    let ads = this.state.AdsArray;
-    let adItem = {
-      title,
-      message,
-      phone,
-      city,
-      dateTimestamp
+    componentDidMount() {
+        this.retrieveFromLocalStorage();
+    }
+    componentDidUpdate() {
+        this.saveToLocalStorage();
+    }
+    // Save AdsArray to the local storage, using "AdsArray" key
+    saveToLocalStorage() {
+        const ads = this.state.AdsArray;
+        localStorage.setItem('AdsArray', JSON.stringify(ads));
+    }
+    // Retrieves / initializes (if no data was retrieved) AdsArray
+    retrieveFromLocalStorage() {
+        let ads = [];
+        let adsStoredValue = localStorage.getItem('AdsArray');
+        //if we were able to get stored values
+        if (adsStoredValue) {
+            ads = JSON.parse(adsStoredValue);
+        }
+        this.setState({ AdsArray: ads });
+    }
+    // Removes ALL entries from local storage AND from state
+    clearLocalStorage() {
+        this.setState({ AdsArray: [] });
+        localStorage.clear();
+    }
+    // Handle form submit, adding new <Ad /> element (with given arguments) to the AdsArray
+    addNewAdItem = (title, message, phone, city, dateTimestamp) => {
+        let ads = this.state.AdsArray;
+        let adItem = {
+            title,
+            message,
+            phone,
+            city,
+            dateTimestamp
+        };
+        ads.push(adItem);
+        this.setState({ AdsArray: ads });
     };
-    ads.push(adItem);
-    this.setState({ AdsArray: ads });
-  };
-  // Handle delete button click, removing an Ad item from AdsArray by its timestamp
-  deleteAdItem = timestamp => {
-    let ads = this.state.AdsArray;
-    ads = ads.filter((val, i, arr) => {
-      return val.dateTimestamp !== timestamp;
-    });
-    this.setState({ AdsArray: ads });
-  };
+    // Handle delete button click, removing an Ad item from AdsArray by its timestamp
+    deleteAdItem = timestamp => {
+        let ads = this.state.AdsArray;
+        ads = ads.filter((val, i, arr) => {
+            return val.dateTimestamp !== timestamp;
+        });
+        this.setState({ AdsArray: ads });
+    };
 
-  // Determines whether AdsArray is empty or not
-  AdsArrayIsEmpty() {
-    return this.state.AdsArray.length === 0;
-  }
+    // Determines whether AdsArray is empty or not
+    AdsArrayIsEmpty() {
+        return this.state.AdsArray.length === 0;
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <Form addItem={this.addNewAdItem} />
-        {!this.AdsArrayIsEmpty() ? (
-          <AdsList
-            ads={this.state.AdsArray}
-            sort={this.sortAds}
-            deleteHandler={this.deleteAdItem}
-          />
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='app'>
+                <Form addItem={this.addNewAdItem} />
+                {!this.AdsArrayIsEmpty() ? (
+                    <AdsList
+                        ads={this.state.AdsArray}
+                        sort={this.sortAds}
+                        deleteHandler={this.deleteAdItem}
+                    />
+                ) : (
+                    ''
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/AdsList/AdsList.js
+++ src/components/AdsList/AdsList.js
@@ -1,36 +1,36 @@
-import React from "react";
-import Ad from "../Ad/Ad.js";
-import "./AdsList.scss";
+import React from 'react';
+import Ad from '../Ad/Ad.js';
+import './AdsList.scss';
 
 const AdsList = ({ ads, deleteHandler }) => {
-  // Sort ads to show the most relevant on top of the AdsList using dateTimestamp key
-  // dateTimestamp is +(new Date()) on a moment form was submitted
-  const sortedAds = () =>
-    ads.sort((a, b) => {
-      const aStamp = a.dateTimestamp;
-      const bStamp = b.dateTimestamp;
-      return bStamp - aStamp;
-    });
+    // Sort ads to show the most relevant on top of the AdsList using dateTimestamp key
+    // dateTimestamp is +(new Date()) on a moment form was submitted
+    const sortedAds = () =>
+        ads.sort((a, b) => {
+            const aStamp = a.dateTimestamp;
+            const bStamp = b.dateTimestamp;
+            return bStamp - aStamp;
+        });
 
-  const renderAds = () =>
-    sortedAds().map(ad => (
-      <Ad
-        key={ad.dateTimestamp}
-        dateTimestamp={ad.dateTimestamp}
-        title={ad.title}
-        message={ad.message}
-        phone={ad.phone}
-        city={ad.city}
-        deleteHandler={deleteHandler}
-      />
-    ));
+    const renderAds = () =>
+        sortedAds().map(ad => (
+            <Ad
+                key={ad.dateTimestamp}
+                dateTimestamp={ad.dateTimestamp}
+                title={ad.title}
+                message={ad.message}
+                phone={ad.phone}
+                city={ad.city}
+                deleteHandler={deleteHandler}
+            />
+        ));
 
-  return (
-    <div className="AdsList">
-      <h2 className="AdsList__title">Объявление</h2>
-      {renderAds()}
-    </div>
-  );
+    return (
+        <div className='AdsList'>
+            <h2 className='AdsList__title'>Объявление</h2>
+            {renderAds()}
+        </div>
+    );
 };
 
 export default AdsList;
